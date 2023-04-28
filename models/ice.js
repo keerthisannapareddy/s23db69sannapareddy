5ddbadf840d714036252e52f9f1af4dbe82559b4
keerthisannapareddy <123145377+keerthisannapareddy@users.noreply.github.com>
2023-04-27 21:50:51 -0500

    Update ice.js

--- models/ice.js
+++ models/ice.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
-const soapsSchema = mongoose.Schema({
+const icesSchema = mongoose.Schema({
   ice_name:  {type:String , required: true, minLength:[1,'itemname']},
   number_of_scoops: { type: Number, required: true,min: 0, max: 60},
   ice_price: {type:String,required: true,maxLength:[8]}, 
 });
 
-module.exports = mongoose.model("soaps", soapsSchema);
+module.exports = mongoose.model("ice", icesSchema);
